af971a495f0e1c31f924356f5dd0a7b9aece7923
Sidharth Rao <sidharthmrao@gmail.com>
2024-03-22 17:28:37 -0400

    Convert to AckermannDriveStamped from Twist

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
+Requires:
+ - ros-noetic-joy
+ - ros-noetic-ackermann_msgs
+
 Please follow the tutorial at
-	http://wiki.ros.org/joy/Tutorials/ConfiguringALinuxJoystick
+ - http://wiki.ros.org/joy/Tutorials/ConfiguringALinuxJoystick
 
 On your computer:
-	export ROS_IP='your_ip_on_LAN'
-	export ROS_MASTER_URI=http://mini-uno:11311
-	sudo chmod a+rw /dev/input/js0
-	rosparam set joy_node/dev "/dev/input/js0"
-	rosrun joy joy_node
+ - export ROS_IP='your_ip_on_LAN'
+ - export ROS_MASTER_URI=http://mini-uno:11311
+ - sudo chmod a+rw /dev/input/js0
+ - rosparam set joy_node/dev "/dev/input/js0"
+ - rosrun joy joy_node
 On the pi:
-	rostopic echo joy
+ - rostopic echo joy
 
--- src/interpret_joy.py
+++ src/interpret_joy.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 import rospy
-from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Joy
-
+from ackermann_msgs.msg import AckermannDriveStamped
 
 def joy_to_twist(data):
 	global max_turning_angle
@@ -11,10 +10,11 @@ def joy_to_twist(data):
 	turn = data.axes[3] * max_turning_angle  # Positive angle turns left, so counterclockwise
 	drive = -.5 * (data.axes[2] - 1) * max_vel  # By default axis is 1, and goes to -1, so transform
 
-	twist = Twist()
-	twist.linear.x = drive  # LT
-	twist.angular.z = turn  # R Stick
-	pub.publish(twist)
+	msg = AckermannDriveStamped()
+	msg.header.stamp = rospy.Time.now()
+	msg.header.steering_angle = turn  # LT
+	msg.header.speed = drive  # R Joy
+	pub.publish(msg)
 
 # Intializes everything
 def start():
@@ -25,7 +25,7 @@ def start():
 	max_vel = 5
 	max_turning_angle = 50
 
-	pub = rospy.Publisher('rc_movement_twist', Twist, queue_size=10)
+	pub = rospy.Publisher('rc_movement_msg', AckermannDriveStamped, queue_size=10)
 
 	rospy.Subscriber("joy", Joy, joy_to_twist)
 
