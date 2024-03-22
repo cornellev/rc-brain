729544b608bd98e355cd03c8dde0df549169f03f
Sidharth Rao <sidharthmrao@gmail.com>
2024-03-22 18:01:41 -0400

    Made drive node, refactor launch files.

--- src/drive_node.py
+++ src/drive_node.py
@@ -1,1 +1,17 @@
-#!/bin/python3
+#!/usr/bin/env python3
+import rospy
+from ackermann_msgs.msg import AckermannDrive
+
+def drive_ackermann(msg):
+	rospy.loginfo("RECEIVED:\n\n")
+	rospy.loginfo(msg.steering_angle)
+	rospy.loginfo(msg.speed)
+
+def start():
+	rospy.Subscriberr"rc_movement_msg", AckermannDrive, drive_ackermann)
+
+	rospy.init_node('drive_node')
+	rospy.spin()
+
+if __name__ == '__main__':
+	start()
