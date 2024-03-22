1c74471f166e7e486b812a6640cb9fcb36745fd0
Sidharth Rao <sidharthmrao@gmail.com>
2024-03-22 13:59:09 -0400

    Added a basic joy interpreter.

--- README.md
+++ README.md
@@ -2,8 +2,10 @@ Please follow the tutorial at
 	http://wiki.ros.org/joy/Tutorials/ConfiguringALinuxJoystick
 
 On your computer:
+	export ROS_MASTER_URI=http://mini-uno:11311
 	sudo chmod a+rw /dev/input/js0
 	rosparam set joy_node/dev "/dev/input/js0"
 	rosrun joy joy_node
 On the pi:
 	rostopic echo joy
+
---
+++ src/interpret_joy.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+import rospy
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import Joy
+
+def scale(axis_value: float):
+	global max_vel
+	global min_vel
+
+	# Scale to max_velocity if positive, scale to min_velocity if negative
+	if axis_value > 0:
+		return min(axis_value ** 1.5 * max_vel, max_vel)
+	return max(abs(axis_value) ** 1.5 * min_vel, min_vel)
+
+def joy_to_twist(data):
+	twist = Twist()
+	twist.linear.x = scale(data.axes[1])  # Left stick
+	twist.angular.z = scale(data.axes[0])  # Right stick
+	pub.publish(twist)
+
+# Intializes everything
+def start():
+	global pub
+	global max_vel
+	global min_vel
+
+	max_vel = 5
+	min_vel = -5
+
+	pub = rospy.Publisher('rc_movement_twist', Twist, queue_size=10)
+
+	rospy.Subscriber("joy", Joy, joy_to_twist)
+
+	rospy.init_node('joy_interpreter')
+	rospy.spin()
+
+if __name__ == '__main__':
+	start()
