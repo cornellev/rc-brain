dcbe03bdba9e91b58e49c6bbb9a57d6127a8d9db
Myles Pasetsky <myles.pasetsky@gmail.com>
2024-03-23 13:07:07 -0400

    Update gamepad io (add some gamepad support)

--- src/interpret_joy.py
+++ src/interpret_joy.py
@@ -12,21 +12,24 @@ class JoyInterpreter:
 		rospy.Subscriber("joy", Joy, self.joy_to_twist)
 
 		self.max_turning_angle = rospy.get_param(param_name='max_turning_angle', default=1.0)
-		self.max_vel = rospy.get_param(param_name='max_vel', default=1.0)
-		rospy.loginfo(f"Found parameter max_turning_angle={self.max_turning_angle}")
-		rospy.loginfo(f"Found parameter max_vel={self.max_vel}")
+		self.max_velocity = rospy.get_param(param_name='max_velocity', default=1.0)
+		
+		if not rospy.has_param('max_turning_angle'): rospy.logwarn("`max_turning_angle` parameter not specified")
+		if not rospy.has_param('max_velocity'): rospy.logwarn("`max_velocity` parameter not specified")
+
+		rospy.loginfo(f"Parameter max_turning_angle={self.max_turning_angle}")
+		rospy.loginfo(f"Parameter max_velocity={self.max_velocity}")
 
 	def joy_to_twist(self, data):
 		gamepad_data = self.joystick_read_type(data.axes, data.buttons)
 		turn = gamepad_data.get_right_stick_x() * self.max_turning_angle  # Positive angle turns left, so counterclockwise
-		drive = gamepad_data.get_left_trigger() * self.max_vel  # By default axis is 1, and goes to -1, so transform
+		drive = (gamepad_data.get_left_trigger() - gamepad_data.get_right_trigger()) * self.max_velocity  # By default axis is 1, and goes to -1, so transform
 
 		msg = AckermannDrive()
-		msg.steering_angle = turn  # LT
-		msg.speed = drive  # R Joy
+		msg.steering_angle = turn
+		msg.speed = drive 
 
-		rospy.loginfo(f"Turn command: {turn}")
-		rospy.loginfo(f"Drive command: {drive}")
+		rospy.loginfo(data.axes)
 
 		self.movement_pub.publish(msg)
 
