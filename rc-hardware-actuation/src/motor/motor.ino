b68084854dab493d55041261750e010ac6779fbc
Sidharth Rao <sidharthmrao@gmail.com>
2024-04-20 02:05:01 -0400

    Fixed motor issue, both work.

--- rc-hardware-actuation/src/motor/motor.ino
+++ rc-hardware-actuation/src/motor/motor.ino
@@ -1,6 +1,18 @@
-#define enA 9
-#define in1 10
-#define in2 11
+#define enA 10
+#define in1 11
+#define in2 12
+
+#define enB 6
+#define in3 9
+#define in4 8
+
+void writeLeft(float value) {
+  analogWrite(enB, value * 255);
+}
+
+void writeRight(float value) {
+  analogWrite(enA, value * 255);
+}
 
 void setup() {
   Serial.begin(9600);
@@ -8,16 +20,25 @@ void setup() {
   pinMode(enA, OUTPUT);
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
+  pinMode(enB, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
 
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, HIGH);
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(enA, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+  digitalWrite(enB, LOW);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // put your main code here, to run repeatedly
 
-  digitalWrite(enA, HIGH);
-  Serial.write("hello");
+  writeLeft(.3);
+  writeRight(.3);
 
+  // analogWrite(enA, 60.5);
+  // analogWrite(enB, 255);
 }
