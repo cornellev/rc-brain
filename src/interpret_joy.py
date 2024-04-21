ec9c72f3970c00d40f0c43fdf941fb752c9a4c09
Sidharth Rao <sidharthmrao@gmail.com>
2024-04-21 15:29:39 -0400

    Add deadzone parameter.

--- src/interpret_joy.py
+++ src/interpret_joy.py
@@ -11,25 +11,30 @@ class JoyInterpreter:
 		self.movement_pub = rospy.Publisher('rc_movement_msg', AckermannDrive, queue_size=10)
 		rospy.Subscriber("joy", Joy, self.joy_to_twist)
 
+		self.deadzone = rospy.get_param(param_name='deadzone', default=0.1)
 		self.max_turning_angle = rospy.get_param(param_name='max_turning_angle', default=1.0)
 		self.max_velocity = rospy.get_param(param_name='max_velocity', default=1.0)
-		
+
+		if not rospy.has_param('deadzone'): rospy.logwarn("`deadzone` parameter not specified")
 		if not rospy.has_param('max_turning_angle'): rospy.logwarn("`max_turning_angle` parameter not specified")
 		if not rospy.has_param('max_velocity'): rospy.logwarn("`max_velocity` parameter not specified")
 
 		rospy.loginfo(f"Parameter max_turning_angle={self.max_turning_angle}")
 		rospy.loginfo(f"Parameter max_velocity={self.max_velocity}")
+		rospy.loginfo(f"Parameter deadzone={self.deadzone}")
 
 	def joy_to_twist(self, data):
 		gamepad_data = self.joystick_read_type(data.axes, data.buttons)
 		turn = gamepad_data.get_right_stick_x() * self.max_turning_angle  # Positive angle turns left, so counterclockwise
+
+		if abs(turn) < self.deadzone:   
+			turn = 0.0
+
 		drive = (gamepad_data.get_left_trigger() - gamepad_data.get_right_trigger()) * self.max_velocity  # By default axis is 1, and goes to -1, so transform
 
 		msg = AckermannDrive()
 		msg.steering_angle = turn
 		msg.speed = drive 
-
-		# rospy.loginfo(data.axes)
 
 		self.movement_pub.publish(msg)
 
