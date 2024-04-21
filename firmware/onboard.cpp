a1d8f343c95dbcfcfbc709dd68c11222426bc293
Sidharth Rao <sidharthmrao@gmail.com>
2024-04-21 14:32:49 -0400

    Added docs, stop movement of vehicle if no messages for a certain amount of time. Reenabled encoder message.

--- firmware/onboard.cpp
+++ firmware/onboard.cpp
@@ -14,19 +14,39 @@
 #define in4 A4
 
 #define servoPin 9
-Servo servo;
 
-const float MAX_SPEED = 1.0;
-const float MIN_SPEED = -1.0;
 
-const float STEERING_ZERO_ANGLE = 110.0;
-const float MAX_INPUT_STEER = 70.0;
+const float MAX_SPEED = 1.0; // Max percent speed of vehicle
+const float MIN_SPEED = -1.0; // Min percent speed
 
+const float STEERING_ZERO_ANGLE = 110.0; // Calibrated servo angle corresponding to a steering angle of 0
+const float MAX_INPUT_STEER = 70.0; // Steering range is from -MAX_INPUT_STEER to MAX_INPUT_STEER
+
+int current_steering_angle; // Current steering angle of the vehicle
+
+long last_message_time; // Time of last message received
+long message_timeout = 1000; // Time in milliseconds without a message before the vehicle stops moving
+
+
+/**
+ * Turn steering servo to specified angle. Angle will be clamped to be between -MAX_INPUT_STEER and MAX_INPUT_STEER.
+ * 
+ * @param angle: Angle to turn the servo to. Positive values turn the wheels to the right, negative values turn the wheels to the left. Zero is straight ahead.
+ */
 void writeAngle(float angle) {
   angle = max(min(STEERING_ZERO_ANGLE + angle, STEERING_ZERO_ANGLE + MAX_INPUT_STEER), STEERING_ZERO_ANGLE - MAX_INPUT_STEER);
   servo.write(angle);
+
+  current_steering_angle = angle + STEERING_ZERO_ANGLE;
 }
 
+
+/**
+ * Set the speed of the vehicle. Positive values move the vehicle forward, negative values move the vehicle backward.
+ * 
+ * @param value: Speed of the vehicle as a percentage of the maximum speed.
+ * @pre -1 <= value <= 1
+ */
 void writePercent(float value) {
   if (value >= 0) {
     digitalWrite(in1, LOW);
@@ -41,46 +61,62 @@ void writePercent(float value) {
     digitalWrite(in2, LOW);
     digitalWrite(in3, HIGH);
     digitalWrite(in4, LOW);
+
     analogWrite(enA, -value * 255);
     analogWrite(enB, -value * 255);
   }
 }
 
+
+/**
+ * Write the steering angle and speed of the vehicle.
+ *
+ * @param angle: Angle to turn the steering servo to. Positive values turn the wheels to the right, negative values turn the wheels to the left. Zero is straight ahead.
+ * @param speed: Speed of the vehicle as a percentage of the maximum speed.
+ * @pre -1 <= speed <= 1
+ */
 void writeAckermann(float angle, float speed) {
   writeAngle(angle);
   writePercent(speed);
 }
 
 
-
-void ackermannDriveCallback(const ackermann_msgs::AckermannDrive& msg)
-{
-  writeAckermann(msg.steering_angle * MAX_INPUT_STEER, msg.speed);
+/**
+ * Callback for ackermann drive messages. Converts the steering angle and speed to the appropriate servo angle and motor speed.
+ */
+void ackermannDriveCallback(const ackermann_msgs::AckermannDrive& msg) {
+  writeAckermann(msg.steering_angle * MAX_INPUT_STEER, msg.speed); 
+  last_message_time = millis(); // Update received last message time
 }
 
+
+// Initialize hardware/sensors
 Encoder left_encoder(2, 5);
 Encoder right_encoder(3, 6);
+Servo servo;
 
+// Setup ROS interface
 ros::NodeHandle nh;
-// rc_localization_odometry::SensorCollect msg;
-
-// ros::Publisher sensor_collect_pub("sensor_collect", &msg);
-ros::Subscriber<ackermann_msgs::AckermannDrive> sub("rc_movement_msg", &ackermannDriveCallback);
-
-long last_time;
+rc_localization_odometry::SensorCollect msg;
+ros::Publisher sensor_collect_pub("sensor_collect", &msg); // Publishes all sensor data
+ros::Subscriber<ackermann_msgs::AckermannDrive> sub("rc_movement_msg", &ackermannDriveCallback); // Listens to ackermann drive messages and drives the vehicle
 
 void setup()
 {
     Serial.begin(9600);
+
+    // Start node
     nh.initNode();
     nh.subscribe(sub);
-    // nh.advertise(sensor_collect_pub);
+    nh.advertise(sensor_collect_pub);
 
     // Helps when using custom topics over rosserial
     nh.negotiateTopics();
 
+    // Initialize servo
     servo.attach(servoPin);
 
+    // Set up motor pins
     pinMode(enA, OUTPUT);
     pinMode(in1, OUTPUT);
     pinMode(in2, OUTPUT);
@@ -88,33 +124,24 @@ void setup()
     pinMode(in3, OUTPUT);
     pinMode(in4, OUTPUT);
 
-    digitalWrite(in1, LOW);
-    digitalWrite(in2, HIGH);
-    digitalWrite(in3, LOW);
-    digitalWrite(in4, HIGH);
-
-    digitalWrite(enA, LOW);
-    digitalWrite(enB, LOW);
-    
+    // Start vehicle at 0 speed and 0 steering angle
     writeAckermann(0, 0);
 }
 
 void loop()
 {
-    // writeAckermann(25, 0.5);
-
-    // Set last time for rate
-    last_time = millis();
     // Populate the message with sensor data
-    // msg.timestamp = millis();
-    // msg.encoder_left = -(int)left_encoder.read();
-    // msg.encoder_right = (int)right_encoder.read();
-    //msg.steering_angle = 57.62; // Placeholder value
+    msg.timestamp = millis();
+    msg.encoder_left =  (int) -left_encoder.read();
+    msg.encoder_right = (int) right_encoder.read();
+    msg.steering_angle = current_steering_angle; // Placeholder value
 
     // Publish the sensor data
-    // sensor_collect_pub.publish(&msg);
+    sensor_collect_pub.publish(&msg);
 
-    // Spin and sleep until time for next loop
+    if (millis() - last_message_time > message_timeout) {
+      writePercent(0); // Stop the vehicle if no message has been received in message_timeout milliseconds
+    }
 
     nh.spinOnce();
 }
