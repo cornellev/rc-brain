ce2b42c3ea458c45de3be091b4bb3b69718be5a7
Sidharth Rao <sidharthmrao@gmail.com>
2024-04-27 14:26:06 -0400

    Open loop control, still in progress.

--- rc_trajectory_follower/src/open_loop.cpp
+++ rc_trajectory_follower/src/open_loop.cpp
@@ -2,36 +2,48 @@
 #include <rc_trajectory_follower/TrajectoryMsg.h>
 #include <ackermann_msgs/AckermannDrive.h>
 
-rc_trajectory_follower::TrajectoryMsg last_trajectory;
 
-ackermann_msgs::AckermannDrive calculate_setpoint(rc_trajectory_follower::TrajectoryMsg trajectory)
+class TrajectoryFollower
 {
-    int index = (ros::Time::now() - trajectory.header.stamp).toSec() / trajectory.dt;
+private:
+    rc_trajectory_follower::TrajectoryMsg last_trajectory;
 
-    ackermann_msgs::AckermannDrive current;
-    current.speed = trajectory.trajectory[index].speed;
-    current.steering_angle = trajectory.trajectory[index].steering_angle;
+public:
+    TrajectoryFollower()
+    {}
 
-    return current;
-}
+    void trajectory_callback(rc_trajectory_follower::TrajectoryMsg trajectory)
+    {
+        last_trajectory = trajectory;
+    }
 
-void trajectory_callback(rc_trajectory_follower::TrajectoryMsg trajectory)
-{
-    last_trajectory = trajectory;
-}
+    ackermann_msgs::AckermannDrive calculate_setpoint()
+    {
+        int index = (ros::Time::now() - last_trajectory.header.stamp).toSec() / last_trajectory.dt.data;
+
+        ackermann_msgs::AckermannDrive current;
+        current.speed = last_trajectory.trajectory[index].speed.data;
+        current.steering_angle = last_trajectory.trajectory[index].steering_angle.data;
+
+        return current;
+    }
+};
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "open_loop_node");
+    ros::init(argc, argv, "trajectory_follower");
     ros::NodeHandle nh("~");
 
-    ros::Subscriber sub = nh.subscribe("trajectory_msg", 10, trajectory_callback);
-    ros::Publisher pub = nh.advertise<ackermann_msgs::AckermannDrive>("rc_movement_msg", 10);
+    TrajectoryFollower follower ();
+
+    ros::Publisher pub = nh.subscribe("trajectory_msg", 10, follower.trajectory_callback);
+    ros::Subscriber sub = nh.advertise<ackermann_msgs::AckermannDrive>("rc_movement_msg", 10)
 
     ros::Rate rate = ros::Rate(10);
 
     while (ros::ok())
     {
+        pub.publish(follower.calculate_setpoint());
         ros::spinOnce();
         rate.sleep();
     }
