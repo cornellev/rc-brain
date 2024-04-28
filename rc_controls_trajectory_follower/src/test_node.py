d066d16141625b63a556ac6ed38b1abe0cc19e0d
Sidharth Rao <sidharthmrao@gmail.com>
2024-04-27 23:51:16 -0400

    Circle code

--- rc_controls_trajectory_follower/src/test_node.py
+++ rc_controls_trajectory_follower/src/test_node.py
@@ -29,22 +29,35 @@ if __name__ == "__main__":
     rospy.init_node("test_node")
 
     p0 = TrajectoryPointMsg()
-    p0.speed = 0.5
+    p0.speed = .3
     p0.steering_angle = 1
 
     p1 = TrajectoryPointMsg()
-    p1.speed = 0.8
-    p1.steering_angle = 0.5
+    p1.speed = .7
+    p1.steering_angle = 1
 
-    msg = TrajectoryMsg()
-    msg.header.stamp = rospy.Time.now()
-    msg.header.frame_id = "map"
-    msg.dt = 0.5
-    msg.trajectory = [p0, p1]
+    p2 = TrajectoryPointMsg()
+    p2.speed = 1
+    p2.steering_angle = 1
+
+    p3 = TrajectoryPointMsg()
+    p3.speed = .7
+    p3.steering_angle = 1
+
+    p4 = TrajectoryPointMsg()
+    p4.speed = .3
+    p4.steering_angle = 1
 
     rate = rospy.Rate(10)
     pub = rospy.Publisher("/trajectory_msg", TrajectoryMsg, queue_size=1)
 
     while not rospy.is_shutdown():
+
+        msg = TrajectoryMsg()
+        msg.header.stamp = rospy.Time.now()
+        msg.header.frame_id = "map"
+        msg.dt = .5
+        msg.trajectory = [p0, p1, p2, p3, p4]
+
         pub.publish(msg)
         rate.sleep()
