9fe5ea1917f1259db06dbb0a118bfe6971eb9210
Sidharth Rao <sidharthmrao@gmail.com>
2024-04-30 17:05:33 -0400

    Dist

--- rc_controls_autobrake/src/autobrake.py
+++ rc_controls_autobrake/src/autobrake.py
@@ -8,7 +8,7 @@ import math
 min_angle_range_to_register = math.radians(10)
 angle_min = math.radians(150)
 angle_max = math.radians(210)
-min_distance_to_register = 2 # m
+min_distance_to_register = 1.5 # m
 
 
 def convert_polar_dist_to_vertical_dist(dist, angle):
