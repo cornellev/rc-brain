0556e95f200de36e40fd9c9391b7b14e23a99b63
Sidharth Rao <sidharthmrao@gmail.com>
2024-05-02 21:49:19 -0400

    Typo

--- rc_controls_autobrake/src/autobrake2.py
+++ rc_controls_autobrake/src/autobrake2.py
@@ -82,7 +82,7 @@ def check_collision(data):
       # rospy.loginfo("TIME TO COLLISION: " + str(time_to_collision))
 
       if time_to_collision < max(AUTOBRAKE_TIME * max(velocity, target_velocity), AUTOBRAKE_TIME) or circum_dist_to_obstacle_angle < .4:
-        rospy.loginfo("MIN OBSTACLE: " + circum_dist_to_obstacle_angle)
+        rospy.loginfo("MIN OBSTACLE: " + str(circum_dist_to_obstacle_angle))
       # # if time_to_collision < AUTOBRAKE_TIME:
         collisions += 1
 
