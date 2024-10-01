638d9fc5d0dcf603c972ea994df489127688c96d
Sidharth Rao <sidharthmrao@gmail.com>
2024-09-30 22:12:04 -0400

    Created main package.

---
+++ autonomy/launch/launch.py
@@ -0,0 +1,13 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+
+def generate_launch_description():
+    return LaunchDescription(
+        [
+        ]
+    )
