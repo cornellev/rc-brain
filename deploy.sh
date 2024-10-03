e7eb7c29e9f268254ddfb9dd4b5d7c117b37a28c
Yey007 <55263178+Yey007@users.noreply.github.com>
2024-10-03 00:25:34 +0000

    it's cooked dude

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,16 @@ TARGET_USER="cev"
 TARGET_IP="mini-uno-v-2"
 IMAGE_NAME="mini-deploy"
 
-echo "Building Docker image."
-docker build -t ${IMAGE_NAME} -f Dockerfile.prod .
+# echo "Building Docker image."
+# docker build -t ${IMAGE_NAME} -f Dockerfile.prod .
 
-echo "Saving and transferring."
-docker save ${IMAGE_NAME} |
-    gzip | 
-    ssh ${TARGET_USER}@${TARGET_IP} -tt `docker load; docker run -t ${IMAGE_NAME}`
+echo "Saving image..."
+docker save ${IMAGE_NAME} -o ${IMAGE_NAME}.tar
+
+echo "Transferring image..."
+rsync -avz --progress ${IMAGE_NAME}.tar rsync://${TARGET_USER}@${TARGET_IP}:/cev_home
+
+echo "Loading image on mini car..."
+ssh -tt ${TARGET_USER}@${TARGET_IP} "docker image rm ${IMAGE_NAME}; docker load < /home/${TARGET_USER}/${IMAGE_NAME}.tar"
+
+# rm ${IMAGE_NAME}.tar
