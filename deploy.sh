975246a4ee75a51f48963a6bc06910b6694c4b15
Yey007 <55263178+Yey007@users.noreply.github.com>
2024-10-04 12:16:00 -0400

    Working!

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,37 @@
 #!/bin/bash
 
 TARGET_USER=cev
-TARGET_CAR=$0
+TARGET_CAR=$1
 IMAGE_NAME=mini-deploy
 CONTAINER_NAME=mini-deployed
 SERVER_NAME=mini-server
 SERVER_PORT=5000
 SERVER_USER=cev
+IMAGE_TAG=$SERVER_NAME:$SERVER_PORT/$SERVER_USER/$IMAGE_NAME
+
+if [ -z "${TARGET_CAR}" ]; then
+    echo "usage: $0 TARGET_CAR"
+    exit 1
+fi
 
 echo "Building image..."
-docker build --platform=linux/arm64 -t $SERVER_NAME:$SERVER_PORT/$IMAGE_NAME -f Dockerfile.prod .
+docker build --platform=linux/arm64 -t $IMAGE_TAG -f Dockerfile.deploy .
+if [ $? -ne 0 ]; then
+    echo "Failed to build image."
+    exit 1
+fi
 
 echo "Pushing to local registry $SERVER_NAME..."
-docker push $SERVER_NAME:$SERVER_PORT/$SERVER_USER/$IMAGE_NAME
+docker push $IMAGE_TAG 
+if [ $? -ne 0 ]; then
+    echo "Failed to push to registry."
+    exit 1
+fi
 
 echo "Pulling down on car $TARGET_CAR and starting..."
-ssh $TARGET_USER@$TARGET_CAR << EOF
-    docker stop $CONTAINER_NAME &&
-    docker container rm $CONTAINER_NAME &&
-    docker pull $SERVER_NAME:$SERVER_PORT/$IMAGE_NAME &&
-    docker run $IMAGE_NAME --name $CONTAINER_NAME --network=host
+ssh -tt $TARGET_USER@$TARGET_CAR << EOF
+    docker stop $CONTAINER_NAME 2>/dev/null || true
+    docker container rm $CONTAINER_NAME 2>/dev/null || true
+    docker pull $IMAGE_TAG && docker run $IMAGE_TAG --name $CONTAINER_NAME --network=host
+    exit 0
 EOF
