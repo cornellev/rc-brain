70f803f67991c0c4006735276a6c8fd31da5515c
Yey007 <55263178+Yey007@users.noreply.github.com>
2024-11-25 21:30:22 -0500

    Fixed stupid problems

--- vision/setup.py
+++ vision/setup.py
@@ -10,6 +10,7 @@ setup(
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
         (f"share/{package_name}/config", ["config/occupancy.yaml"]),
+        (f"share/{package_name}/launch", ["launch/launch.py"]),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
--- vision/vision/occupancy_transformer.py
+++ vision/vision/occupancy_transformer.py
@@ -3,29 +3,38 @@ import struct
 import numpy as np
 import numpy.typing as npt
 import rclpy
+import rclpy.node
+import rclpy.qos
 import rclpy.time
 import tf2_ros
 from geometry_msgs.msg import Point, Pose, Quaternion
 from nav_msgs.msg import OccupancyGrid
-from rclpy import Node
-from rclpy.qos import QoSPresetProfiles
 from sensor_msgs.msg import PointCloud2, PointField
 
 
-class OccupancyTransformerNode(Node):
+class OccupancyTransformerNode(rclpy.node.Node):
     def __init__(self):
         super().__init__("occupancy_transformer")
-        self.subscription = self.create_subscription(
-            PointCloud2, "point_cloud", self.pc_callback, QoSPresetProfiles.SENSOR_DATA
-        )
-        self.publisher = self.create_publisher(OccupancyGrid, "occupancy_grid", 10)
 
+        self.declare_parameter("point_cloud_topic", "point_cloud")
         self.declare_parameter("map_height_m", 10.0)
         self.declare_parameter("map_width_m", 10.0)
         self.declare_parameter("map_resolution_m", 0.1)
+        self.declare_parameter("ride_height_m")
+        self.declare_parameter("car_height_m")
+        self.declare_parameter("fuzz_threshold_m")
+        self.declare_parameter("base_link_frame", "base_link")
+
+        self.subscription = self.create_subscription(
+            PointCloud2,
+            self.get_parameter("point_cloud_topic").value,
+            self.pc_callback,
+            rclpy.qos.qos_profile_sensor_data,
+        )
+        self.publisher = self.create_publisher(OccupancyGrid, "occupancy", 10)
 
         self.tf_buffer = tf2_ros.Buffer()
-        self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
+        self.tf_listener = tf2_ros.TransformListener(self.tf_buffer, self)
 
     def pc_callback(self, cloud: PointCloud2):
         map_height_m = self.get_parameter("map_height_m").value
@@ -35,15 +44,10 @@ class OccupancyTransformerNode(Node):
         map_height_cells = int(map_height_m / map_resolution_m)
         map_width_cells = int(map_width_m / map_resolution_m)
 
-        ride_height_m = self.get_parameter(
-            "ride_height_m"
-        ).value  # height of base link above ground
-        car_height_m = self.get_parameter(
-            "car_height_m"
-        ).value  # total height of car above ground
+        ride_height_m = self.get_parameter("ride_height_m").value
+        car_height_m = self.get_parameter("car_height_m").value
         fuzz_threshold_m = self.get_parameter("fuzz_threshold_m").value
 
-        # transform point cloud to base_link reference
         base_link_frame = self.get_parameter("base_link_frame").value
         transform = self.tf_buffer.lookup_transform(
             base_link_frame, cloud.header.frame_id, rclpy.time.Time()
@@ -56,20 +60,14 @@ class OccupancyTransformerNode(Node):
         )
 
         point_cloud = parse_points(cloud)
-        base_link_pc = rotation * point_cloud + translation
+        base_link_pc = point_cloud @ rotation.T + translation
 
-        # filter for z that matters (we will run into it)
         pc_z = base_link_pc[:, 2]
         filtered_pc = base_link_pc[
             (pc_z < car_height_m + fuzz_threshold_m)
             & (pc_z > ride_height_m - fuzz_threshold_m)
         ]
 
-        # center base_link at cell (0, map_width_cells // 2) facing down the grid
-        # we will set the origin accordingly
-        # x grows down the array and y grows across the array
-        # cell_x = pc_x // map_resolution
-        # cell_y = (pc_y + map_width_m / 2) // map_resolution
         grid = np.zeros((map_height_cells, map_width_cells), dtype=np.int8)
 
         cell_x = filtered_pc[:, 0] / map_resolution_m
@@ -98,15 +96,14 @@ class OccupancyTransformerNode(Node):
         occupancy_grid.info.width = map_width_cells
         occupancy_grid.info.height = map_height_cells
 
-        # relative to base link, which we're placing at (0, map_width_cells // 2)
-        origin_offset = (-map_width_cells * map_resolution_m) // 2
+        origin_offset = (-map_width_cells * map_resolution_m) / 2
         map_origin = Pose(
-            position=Point(x=0, y=origin_offset, z=0),
-            orientation=Quaternion(x=0, y=0, z=0, w=1),
+            position=Point(x=0.0, y=origin_offset, z=0.0),
+            orientation=Quaternion(x=0.0, y=0.0, z=0.0, w=1.0),
         )
         occupancy_grid.info.origin = map_origin
 
-        occupancy_grid.data = flat
+        occupancy_grid.data = list(map(int, flat))
 
         self.publisher.publish(occupancy_grid)
 
@@ -116,9 +113,9 @@ def parse_points(cloud: PointCloud2) -> npt.NDArray[np.float32]:
     (x_field,) = [field for field in cloud.fields if field.name == "x"]
     (y_field,) = [field for field in cloud.fields if field.name == "y"]
     (z_field,) = [field for field in cloud.fields if field.name == "z"]
-    x_format = get_unpack_format(x_field)
-    y_format = get_unpack_format(y_field)
-    z_format = get_unpack_format(z_field)
+    x_format = get_unpack_format(x_field, cloud.is_bigendian)
+    y_format = get_unpack_format(y_field, cloud.is_bigendian)
+    z_format = get_unpack_format(z_field, cloud.is_bigendian)
 
     n_points = len(cloud.data) // cloud.point_step
     result = np.zeros((n_points, 3), dtype=np.float32)
@@ -126,13 +123,13 @@ def parse_points(cloud: PointCloud2) -> npt.NDArray[np.float32]:
     # TODO: this probably needs to get faster using iter_unpack and pad bytes
     for i in range(n_points):
         point_offset = i * cloud.point_step
-        result[i][0] = struct.unpack_from(
+        (result[i][0],) = struct.unpack_from(
             x_format, buffer=cloud.data, offset=point_offset + x_field.offset
         )
-        result[i][1] = struct.unpack_from(
+        (result[i][1],) = struct.unpack_from(
             y_format, buffer=cloud.data, offset=point_offset + y_field.offset
         )
-        result[i][2] = struct.unpack_from(
+        (result[i][2],) = struct.unpack_from(
             z_format, buffer=cloud.data, offset=point_offset + z_field.offset
         )
 
@@ -142,24 +139,19 @@ def parse_points(cloud: PointCloud2) -> npt.NDArray[np.float32]:
 def get_unpack_format(field: PointField, big_endian: bool):
     endian_format = ">" if big_endian else "<"
     count_format = str(field.count)
-    type_format = ""
-    match field.datatype:
-        case PointField.INT8:
-            type_format = "b"
-        case PointField.UINT8:
-            type_format = "B"
-        case PointField.INT16:
-            type_format = "h"
-        case PointField.UINT16:
-            type_format = "H"
-        case PointField.INT32:
-            type_format = "i"
-        case PointField.UINT32:
-            type_format = "I"
-        case PointField.FLOAT32:
-            type_format = "f"
-        case PointField.FLOAT64:
-            type_format = "d"
+
+    type_format_dict = {
+        PointField.INT8: "b",
+        PointField.UINT8: "B",
+        PointField.INT16: "h",
+        PointField.UINT16: "H",
+        PointField.INT32: "i",
+        PointField.UINT32: "I",
+        PointField.FLOAT32: "f",
+        PointField.FLOAT64: "d",
+    }
+    type_format = type_format_dict.get(field.datatype, "")
+
     return f"{endian_format}{count_format}{type_format}"
 
 
