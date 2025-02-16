1a815de09f59dbd32a76aed3f3dd891652106086
Sidharth Rao <sidharthmrao@gmail.com>
2025-02-16 01:56:17 -0500

    Fix slam toolbox installation.

--- install.sh
+++ install.sh
@@ -1,2 +1,7 @@
+#!/bin/bash
+
 sudo apt-get install libqt5serialport5-dev
+sudo apt install libsuitesparse-dev
+sudo apt install libceres-dev
+sudo apt install ros-humble-slam-toolbox 
 git submodule update --init --recursive
